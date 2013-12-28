e24dde533dc4d54c37c2d6a8bda179b3f1026e08
Matt Thomas <matt@betweenbrain.com>
2013-12-28 10:54:14 -0500

    Remove sample data from install.sql

--- component/admin/install.sql
+++ component/admin/install.sql
@@ -7,5 +7,3 @@ CREATE TABLE IF NOT EXISTS `#__google_codes` (
   `publish_down` datetime DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-INSERT IGNORE INTO `#__google_codes` (`id`, `url`, `code`, `published`) VALUES ('1','http://foo.com','Test Foo','0'), ('2', 'http://bar.com', 'Test Bar', '1'), ('3', 'http://foo.com/baz/*', 'Test Foo Wildcard', '-1');
